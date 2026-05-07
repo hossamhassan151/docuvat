ef25873c916149d2986d48186ef8048e8170115e
hossam <hm.1511996@gmail.com>
2026-05-07 16:56:02 +0400

    update clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,15 @@ export default function RootLayout({
       gtag('config', 'G-HNE73B20HF');
     `}
   </Script>
+  <Script id="clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wne0jag5do");
+  `}
+</Script>
 </body>
     </html>
   );
